83464ddeb58bf15dcfac1a1a91bff098c0fa113a
Cole Allan <cacoleallan@gmail.com>
2016-12-22 12:02:43 -0700

    All gulp files from lecture

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,9 @@
 angular.module("app").controller("ctrl", function($scope, service){
-   
+
     $scope.thisappisbroken = "This app is working";
 
     $scope.germData = service.getGerms();
-    
+
+    $scope.doSomething = () => 7;
+
 });
